6043c2df8c0e684f2de73dda07ddd1d2d82b15e3
santiago23espinosa <santiago23espinosa@gmail.com>
2024-09-22 16:11:06 -0500

    9

---
+++ index.js
@@ -0,0 +1,9 @@
+const productos = [
+    { nombre: 'Paracetamol', precioAnterior: 7.99, precioActual: 5.99, descuento: true },
+    { nombre: 'Ibuprofeno', precioAnterior: 9.99, precioActual: 7.99, descuento: true },
+    { nombre: 'Vitamina C', precioAnterior: 12.99, precioActual: 9.99, descuento: true },
+    { nombre: 'Alcohol en gel', precioAnterior: 4.99, precioActual: 3.99, descuento: true },
+    { nombre: 'Aspirina', precio: 6.99, descuento: false },
+    { nombre: 'Omeprazol', precio: 8.99, descuento: false },
+    { nombre: 'Loratadina', precio: 7.99, descuento: false },
+    { nombre: 'Multivitamínico', precio: 14.99, descuento: false }
