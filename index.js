e63af11d0c9029a7ae73351cf1e1f087e443a097
santiago23espinosa <santiago23espinosa@gmail.com>
2024-09-22 16:14:01 -0500

    10

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const productos = [
-    { nombre: 'Paracetamol', precioAnterior: 7.99, precioActual: 5.99, descuento: true },
-    { nombre: 'Ibuprofeno', precioAnterior: 9.99, precioActual: 7.99, descuento: true },
-    { nombre: 'Vitamina C', precioAnterior: 12.99, precioActual: 9.99, descuento: true },
-    { nombre: 'Alcohol en gel', precioAnterior: 4.99, precioActual: 3.99, descuento: true },
-    { nombre: 'Aspirina', precio: 6.99, descuento: false },
-    { nombre: 'Omeprazol', precio: 8.99, descuento: false },
-    { nombre: 'Loratadina', precio: 7.99, descuento: false },
-    { nombre: 'Multivitamínico', precio: 14.99, descuento: false }
+    { nombre: 'Paracetamol', precioAnterior: 7.900, precioActual: 5.900, descuento: true },
+    { nombre: 'Ibuprofeno', precioAnterior: 9.900, precioActual: 7.900, descuento: true },
+    { nombre: 'Vitamina C', precioAnterior: 12.500, precioActual: 9.500, descuento: true },
+    { nombre: 'Alcohol en gel', precioAnterior: 4.800, precioActual: 3.800, descuento: true },
+    { nombre: 'Aspirina', precio: 6.900, descuento: false },
+    { nombre: 'Omeprazol', precio: 8.300, descuento: false },
+    { nombre: 'Loratadina', precio: 7.500, descuento: false },
+    { nombre: 'Multivitamínico', precio: 14.00, descuento: false }
+];
+function mostrarProductos(productosAMostrar) {
+    const contenedor = document.querySelector('.producto-container');
+    contenedor.innerHTML = ''; // Limpiar contenedor
+
+    productosAMostrar.forEach(producto => {
+        const productoElement = document.createElement('div');
+        productoElement.classList.add('producto');
+        
+        if (producto.descuento) {
+            productoElement.innerHTML = `
+                <h3>${producto.nombre}</h3>
+                <p class="precio-anterior">$${producto.precioAnterior.toFixed(2)}</p>
+                <p class="precio-actual">$${producto.precioActual.toFixed(2)}</p>
+            `;
+        } else {
+            productoElement.innerHTML = `
+                <h3>${producto.nombre}</h3>
+                <p class="precio-actual">$${producto.precio.toFixed(2)}</p>
+            `;
+        }
+        
+        contenedor.appendChild(productoElement);
+    });
+}
